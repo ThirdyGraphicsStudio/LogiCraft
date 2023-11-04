4ef4aae6270cc464bbec32d50bb671fd1c3d097a
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-11-04 17:35:57 +0800

    add Main Truth Table

---
+++ app/src/main/java/com/thirdy/booleanexpression/MainTruthTable.java
@@ -0,0 +1,14 @@
+package com.thirdy.booleanexpression;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainTruthTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.truth_table_main);
+    }
+}
